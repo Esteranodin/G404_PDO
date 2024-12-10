2bd6024bb3833b140550947cc9a1f0c0abf9c0c3
esteranodin <esteranodin@hotmail.fr>
2024-12-10 12:35:08 +0100

    DONE: exo3

--- Partie_2-patients/profil-patient.php
+++ Partie_2-patients/profil-patient.php
@@ -1,16 +1,17 @@
 <?php
 require_once './utils/connect.php';
 
-// $sql = "SELECT * FROM `patients` WHERE `id` = :id";
+$id = $_POST['idPatient'];
+$sql = "SELECT * FROM `patients` WHERE `id` = $id";
 
-// try {
-//     $stmt = $pdo->query($sql);
-//     $patients = $stmt->fetchAll(PDO::FETCH_ASSOC);
-// } catch (PDOException $error) {
-//     echo "Erreur lors de la requete : " . $error->getMessage();
-// }
+try {
+    $stmt = $pdo->query($sql);
+    $patients = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $error) {
+    echo "Erreur lors de la requete : " . $error->getMessage();
+}
 
-// ?>
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -23,12 +24,16 @@ require_once './utils/connect.php';
 </head>
 
 <body>
+    <?php
+    foreach ($patients as $patient) {
+    ?>
+        <li>Nom : <?= $patient['lastname']  ?> <br>
+            Prénom : <?= $patient['firstname']  ?>
+        <?php
+    }
 
-<?php 
-var_dump($patient['id']);
-die;
-?>
-<a href="./liste-patients.php">Retour à la liste des patients </a>
+        ?>
+        <a href="./liste-patients.php">Retour à la liste des patients </a>
 
 </body>
 
